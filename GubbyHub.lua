8be6df8b83192afed01118c901b530c06b8c1cc7
CatbyteHD <m66361544@gmail.com>
2025-08-02 19:17:25 +0300

    Update GubbyHub.lua

--- GubbyHub.lua
+++ GubbyHub.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local Window = Rayfield:CreateWindow({
-        Name = "Gubby Hub™︱Official",
-        LoadingTitle = "Gubby Hub™︱Official",
+        Name = "Gubby Hub︱Official",
+        LoadingTitle = "Gubby Hub︱Official",
         LoadingSubtitle = "By CatbyteHD",
         Theme = "Ocean",
         ToggleUIKeybind = Enum.KeyCode.B,
