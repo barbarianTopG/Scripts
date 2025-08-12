a02bdf30abf9bc01cb42276af84b3bb8604841bf
CatbyteHD <m66361544@gmail.com>
2025-08-12 18:10:38 +0300

    Update LocalThing.lua

--- LocalThing.lua
+++ LocalThing.lua
@@ -2,14 +2,14 @@
 
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Thanks For Executing My Script! For Just A Baseplate.")
 
-wait("1")
+task.wait(1)
 
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("now here are the hats!")
 
-wait("1")
+task.wait(1)
 
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("made by @RC7USER00!")
 
-wait("1")
+task.wait(1)
 
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-ch")
