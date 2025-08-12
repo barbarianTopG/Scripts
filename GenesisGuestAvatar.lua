255982e824817254b3d1d07a383863f835b20582
CatbyteHD <m66361544@gmail.com>
2025-08-12 16:10:56 +0300

    Update GenesisGuestAvatar.lua

--- GenesisGuestAvatar.lua
+++ GenesisGuestAvatar.lua
@@ -1,3 +1,7 @@
 ----====== I made this for myself ======----
 
+
+
+----====== Genesis rigs(Guest) ======----
+
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("/e -gh 48474313, 13058406993, 138364679836274, 131385506535381, 85392395166623, 129462518582032, 106249329428811, 108224319902592, 82404150383568, 100856932339214 ")
