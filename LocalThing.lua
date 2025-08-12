c32edf9057011acaf106c5d136d5481942607f7c
CatbyteHD <m66361544@gmail.com>
2025-08-12 18:07:11 +0300

    Create LocalThing.lua

---
+++ LocalThing.lua
@@ -0,0 +1,15 @@
+-- Made this for my friend (Aka RC7USER00)
+
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Thanks For Executing My Script! For Just A Baseplate.")
+
+wait("1")
+
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("now here are the hats!")
+
+wait("1")
+
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("made by @RC7USER00!")
+
+wait("1")
+
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-ch")
