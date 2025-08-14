879565ab482c91379223bb772a899dd2059137f1
ChillbyteHD <m66361544@gmail.com>
2025-08-14 09:01:07 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,8 +1,8 @@
 --- // == Blacklisted players == \\ ---
 
 --[[ // Reasons I blacklist people:
-1.They try to flijg me.
-2.They are Russian, and I'm with Ukraine ]]
+They try to fling me, OR they are Russian, and I'm with Ukraine.
+]]
 
 return {
     "jimballll",
