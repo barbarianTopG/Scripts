ef269af7218e8e403f59250e565a653340c74693
CatbyteHD <m66361544@gmail.com>
2025-08-14 08:18:55 +0300

    Create Blacklist.lua

---
+++ Blacklist.lua
@@ -0,0 +1,39 @@
+--- // ==== Blacklist ==== \\ ---
+local Players = game:GetService("Players")
+
+local function notif(str, dur)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Service";
+        Text = str;
+        Duration = dur or 3;
+    })
+end
+
+local TARGET_USERNAMES = loadstring(game:HttpGet("https://yourdomain.com/blacklist.lua"))()
+
+local TAG_NAME = "Blacklisted"
+local TargetLookup = {}
+
+for _, name in pairs(TARGET_USERNAMES) do
+    TargetLookup[name] = true
+end
+
+local function Blacklist(player)
+    if player.Character then
+        if player.Character:FindFirstChild("Head") and player.Character.Head:FindFirstChild(TAG_NAME) then
+            return
+        end
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "From ChillbyteHD";
+            Text = "You Are Blacklisted by the owner, sorry! :(";
+            Duration = 3;
+        })
+    end
+end
+
+for _, player in pairs(Players:GetPlayers()) do
+    if TargetLookup[player.Name] then
+        Blacklist(player)
+        return
+    end
+end
