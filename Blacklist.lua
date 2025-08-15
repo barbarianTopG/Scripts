1c2b7814abf5f3081cb1f1113041339840f71bf8
ChillbyteHD <m66361544@gmail.com>
2025-08-15 14:49:52 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,16 +1,21 @@
---- // == Blacklisted players == \\ ---
+--- // ==== Blacklisted players ==== \\ ---
 
---[[
-// Reasons I blacklist people \\
-They try to fling me, OR they are russian.
-]]
+--- // ==== Bozo list ==== \\ ---
 
 return {
-    "jimballll",
-    "Palmen_g",
-    "Milpi_los",
-    "kakish_nega228",
-    "Kesha_Tank",
-    "2tallker64",
-    "w3p1k1saa"
+"jimballll",
+"Palmen_g",
+"Milpi_los",
+"kakish_nega228",
+"Kesha_Tank",
+"2tallker64",
+"w3p1k1saa",
+"sasha2017187",
+"skinr228",
+"windy38830",
+"Radiant_Rose380",
+"miha_gg12c",
+"magistr_shluh",
+"america_xddd8",
+"koffe_123455lox"
 }
