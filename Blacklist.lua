1b90835bdf7a4284eb24265c0ab490f308208ff8
ChillbyteHD <m66361544@gmail.com>
2025-08-15 14:55:03 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -4,5 +4,5 @@ return {
  "jimballll", "Palmen_g", "Milpi_los", "kakish_nega228",
  "Kesha_Tank", "2tallker64", "w3p1k1saa", "sasha2017187",
  "skinr228", "windy38830", "Radiant_Rose380", "miha_gg12c",
- "magistr_shluh", "america_xddd8", "koffe_123455lox"
+ "magistr_shluh", "america_xddd8", "koffe_123455lox", "BobloxPlayer59"
 }
