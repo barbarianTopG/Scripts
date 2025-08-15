bcf3edbe2a792886e38101d352ea7cc50a1b78ce
ChillbyteHD <m66361544@gmail.com>
2025-08-15 14:51:48 +0300

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -2,8 +2,5 @@
 -- // ==== Bozo list ==== \\
 
 return {
- "jimballll", "Palmen_g", "Milpi_los", "kakish_nega228",
- "Kesha_Tank", "2tallker64", "w3p1k1saa", "sasha2017187",
- "skinr228", "windy38830", "Radiant_Rose380", "miha_gg12c",
- "magistr_shluh", "america_xddd8", "koffe_123455lox"
+"jimballll", "Palmen_g", "Milpi_los", "kakish_nega228", "Kesha_Tank", "2tallker64", "w3p1k1saa", "sasha2017187", "skinr228", "windy38830", "Radiant_Rose380", "miha_gg12c", "magistr_shluh", "america_xddd8", "koffe_123455lox"
 }
