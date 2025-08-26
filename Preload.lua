f7d7b93f8d0104040679b0584de0fcff46c2cbc8
ChillbyteHD <m66361544@gmail.com>
2025-08-26 14:14:13 +0300

    Create Preload.lua

---
+++ Preload.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/refs/heads/main/Blacklist.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/main/DevTag.lua"))()
