d77548ba5b11a4d4223cca306aa6863aa3a5fe5b
ChillbyteHD <m66361544@gmail.com>
2025-08-31 10:56:31 +0300

    update KrystalDance.lua

--- KrystalDance.lua
+++ KrystalDance.lua
@@ -8,6 +8,8 @@ end
 
 notify("Local Hub", "Itz epik time")
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/Reanimate.lua"))()
+task.wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/KDV3-Fixed/refs/heads/main/KrystalDance3"))()
 task.wait(2)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/refs/heads/main/SelectKeyboard.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/SelectKeyboard.lua"))()
