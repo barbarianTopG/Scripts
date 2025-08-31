545938f634c7b7b81487d1fe4bfa917a3f2c30b2
ChillbyteHD <m66361544@gmail.com>
2025-08-31 10:19:29 +0300

    update KrystalDance.lua

--- KrystalDance.lua
+++ KrystalDance.lua
@@ -11,4 +11,3 @@ notify("Local Hub", "If people cannot see this use a reanimation, e.g: Just a Ba
 loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/KDV3-Fixed/refs/heads/main/KrystalDance3"))()
 task.wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/refs/heads/main/SelectKeyboard.lua"))()
-end
