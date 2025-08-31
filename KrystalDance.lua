b127c84deb64eb8bce688291eb97a5703fa7f7e4
ChillbyteHD <m66361544@gmail.com>
2025-08-31 11:29:31 +0300

    update KrystalDance.lua

--- KrystalDance.lua
+++ KrystalDance.lua
@@ -6,17 +6,14 @@ function notify(title, text, duration)
     })
 end
 
-local function Send(message)
-    game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(message)
-end
-
 notify("Local Hub", "Itz epik time")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/Reanimate.lua"))()
-task.delay(6, function()
-    Send("-net")
-end)
+
 task.wait(10)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/KDV3-Fixed/refs/heads/main/KrystalDance3"))()
+
 task.wait(2)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/SelectKeyboard.lua"))()
